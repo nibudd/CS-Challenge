ac35992c91f396805bc9f3a8df362e6f8d65359a
nibudd <nibudd@gmail.com>
2020-03-18 10:08:36 -0400

    created MenuItem class to handle console interactions

---
+++ ConsoleApp1/Menus.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+	public abstract class MenuItem
+	{
+		private string promptText;
+		private string userInput;
+		private Dictionary<string, object> validInputDict;
+		private object result;
+		private bool itemCompleted;
+
+		public MenuItem(string promptText, Dictionary<string, object> validInputDict)
+		{
+			this.promptText = promptText;
+			this.validInputDict = validInputDict;
+			itemCompleted = false;
+		}
+
+		public void Execute()
+		{
+			itemCompleted = false;
+			while (!itemCompleted)
+			{
+				PrintPrompt();
+				GetUserInput();
+				if (IsValidInput())
+				{
+					InterpretInput();
+					itemCompleted = true;
+				}
+			}
+		}
+
+		private object GetResult()
+		{
+			return result;
+		}
+
+		private void PrintPrompt()
+		{
+			if (promptText != "")
+				Console.Write(promptText);
+		}
+
+		private void GetUserInput()
+		{
+			string userInput = Console.ReadLine();
+			if (GetValidInputs().Contains(userInput))
+				this.userInput = userInput;
+			else
+				InvalidUserInput();
+		}
+
+		private void InterpretInput()
+		{
+			result = validInputDict[userInput];
+		}
+
+		private List<string> GetValidInputs()
+		{
+			return new List<string>(validInputDict.Keys);
+		}
+
+		private void InvalidUserInput()
+		{
+			Console.WriteLine("Invalid input.");
+			userInput = "";
+		}
+
+		private bool IsValidInput()
+		{
+			return (userInput == "") ? false : true;
+		}
+	}
+}
