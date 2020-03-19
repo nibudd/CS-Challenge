52603fed42961aa50fdf97f223f06cd42ef22450
nibudd <nibudd@gmail.com>
2020-03-19 09:45:52 -0400

    moved interactions the the chucknorris api to JokeHandler class

---
+++ ConsoleApp1/JokeHandler.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class JokeHandler
+	{
+        private int maxQuantity;
+        private JsonFeed chuckNorrisFeed =
+            new JsonFeed("https://api.chucknorris.io");
+        private List<string> jokeCategories;
+
+        public JokeHandler(int maxQuantity)
+        {
+            this.maxQuantity = maxQuantity;
+        }
+
+        public List<string> GetJokes(string jokeCategory, bool usesCategory)
+        {
+            List<string> jokes = new List<string>();
+            string url = "jokes/random";
+            if (usesCategory)
+                url += $"?category={jokeCategory}";
+
+            foreach (int _ in Enumerable.Range(1, maxQuantity))
+            {
+                JObject result = chuckNorrisFeed.GetResponse(url);
+                jokes.Add(result.Value<string>("value"));
+            }
+
+            return jokes;
+        }
+
+        public void GetCategories()
+        {
+            JArray result = chuckNorrisFeed.GetResponse("jokes/categories");
+            jokeCategories = result.ToObject<List<string>>();
+        }
+
+        public string CategoriesToString()
+        {
+            string categoriesString = "";
+            int counter = 0;
+            foreach (string category in jokeCategories)
+                categoriesString += $"{++counter}. {category}\n";
+            return categoriesString;
+        }
+
+        public int CategoryCount()
+        {
+            return jokeCategories.Count();
+        }
+
+        public List<string> ListCategories()
+        {
+            return jokeCategories;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,12 +9,10 @@ namespace ConsoleApp1
     {
         static bool usesRandomName = false;
         static bool usesCategory = false;
-        static List<string> jokeCategories;
         static string jokeCategory;
         static int jokeQuantity;
         static int maxQuantity = 9;
         static bool wantsMoreJokes = true;
-        static JsonFeed chuckNorrisFeed = new JsonFeed("https://api.chucknorris.io");
         private static MenuItem menuUseRandomName;
         private static MenuItem menuUseCategory;
         private static MenuItem menuChooseCategory;
@@ -25,10 +23,11 @@ namespace ConsoleApp1
         private static Dictionary<string, string> categoriesInputDict;
         private static List<string> jokes;
         private static NameSwapper nameSwapper = new NameSwapper("Chuck", "Norris", "male");
+        private static JokeHandler jokeHandler = new JokeHandler(maxQuantity);
 
         static void Main(string[] args)
         {
-            GetCategories();
+            jokeHandler.GetCategories();
             MakeInputDictionaries();
             MakeMenuItems();
 
@@ -42,7 +41,7 @@ namespace ConsoleApp1
                 if (usesCategory)
                     jokeCategory = RunMenuItem<string>(menuChooseCategory, categoriesInputDict);
 
-                GetJokes();
+                jokes = jokeHandler.GetJokes(jokeCategory, usesCategory);
 
                 usesRandomName = RunMenuItem<bool>(menuUseRandomName, yesNoInputDict);
 
@@ -66,21 +65,6 @@ namespace ConsoleApp1
             return inputDict[menuItem.GetInput()];
         }
 
-        private static void GetJokes()
-        {
-            jokes = new List<string>();
-            string url = "jokes/random";
-            if (usesCategory)
-                url += $"?category={jokeCategory}";
-
-            foreach (int _ in Enumerable.Range(1, maxQuantity))
-            {
-                JObject result = chuckNorrisFeed.GetResponse(url);
-                jokes.Add(result.Value<string>("value"));
-            }
-
-        }
-
         private static void PrintJokes()
         {
             foreach (int x in Enumerable.Range(1, jokeQuantity))
@@ -95,7 +79,7 @@ namespace ConsoleApp1
             menuUseCategory = new MenuItem(
                 "Specify a category? y/n: ", new List<string>(yesNoInputDict.Keys));
 
-            string chooseCategoryString = CategoriesToString() + "Select a category: ";
+            string chooseCategoryString = jokeHandler.CategoriesToString() + "Select a category: ";
             menuChooseCategory = new MenuItem(
                 chooseCategoryString, new List<string>(categoriesInputDict.Keys));
 
@@ -116,8 +100,8 @@ namespace ConsoleApp1
         private static Dictionary<string, string> MakeCategoriesDict()
         {
             Dictionary<string, string> categoriesInputDict = new Dictionary<string, string>();
-            foreach (int x in Enumerable.Range(0, jokeCategories.Count()))
-                categoriesInputDict[(x+1).ToString()] = jokeCategories[x];
+            foreach (int x in Enumerable.Range(0, jokeHandler.CategoryCount()))
+                categoriesInputDict[(x+1).ToString()] = jokeHandler.ListCategories()[x];
             return categoriesInputDict;
         }
 
@@ -135,21 +119,6 @@ namespace ConsoleApp1
             yesNoInputDict["y"] = true;
             yesNoInputDict["n"] = false;
             return yesNoInputDict;
-        }
-
-        private static void GetCategories()
-        {
-            JArray result = chuckNorrisFeed.GetResponse("jokes/categories");
-            jokeCategories = result.ToObject<List<string>>();
-        }
-
-        private static string CategoriesToString()
-        {
-            string categoriesString = "";
-            int counter = 0;
-            foreach (string category in jokeCategories)
-                categoriesString += $"{++counter}. {category}\n";
-            return categoriesString;
         }
     }
 }
